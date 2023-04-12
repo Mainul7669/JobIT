56d231fca65af69fdbc37a0fc68f92b4945c8e93
Mainul7669 <mainul7669@gmail.com>
2023-04-12 22:45:11 +0600

    path added

---
+++ main.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+// bootstrap css link
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Main from './Component/Layout/Main';
+import Home from './Component/Home/Home';
+import ApliedJobs from './Component/ApliedJobs/ApliedJobs';
+import Blog from './Component/Blog/Blog';
+import JobDetails from './Component/JobDetails/JobDetails';
+import Statistics from './Component/Statistics/Statistics';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+
+    children: [
+      {
+        path: '',
+        element: <Home></Home>,
+        loader: () => fetch('CategoryData.json')
+      },
+      {
+        path: 'jobDetails/:jobId',
+        element: <JobDetails></JobDetails>,
+      },
+      {
+        path: '/appliedjobs',
+        element: <ApliedJobs></ApliedJobs>,
+
+      },
+      {
+        path: '/blog',
+        element: <Blog></Blog>
+      },
+      {
+        path: '/blog ',
+        element: <Blog></Blog>
+      },
+      {
+        path: '/statistics',
+        element: <Statistics></Statistics>
+      },
+      {
+        path: '*',
+        element: <div className='text-center mt-5 text-bg-danger p-5 ' style={{ height: '300px', alignItems: 'center' }}>
+          <h1 className='p-5 '>404 Not Found</h1>
+        </div>
+      },
+
+
+    ]
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
