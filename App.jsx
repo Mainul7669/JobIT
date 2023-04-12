66271c0ba49f853e0ddb2e30cf32149aa3a1cb8d
Mainul7669 <mainul7669@gmail.com>
2023-04-12 22:51:19 +0600

    css file added

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>asga</h1>
+      <img src="../assets/All Images/tesla-9 1.png" alt="" />
+    </div>
+  )
+}
+
+export default App
